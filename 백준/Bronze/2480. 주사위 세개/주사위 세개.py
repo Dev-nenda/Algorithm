34fbbdbac11579dd17ae14a29047b1491a1e49cc
Dev-nenda <131946370+Dev-nenda@users.noreply.github.com>
2024-01-24 18:42:59 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,19 @@
+# 주사위 3개
+# 같은 눈 3개가 나오면 10000 + 같은눈x1000
+# 같은 눈 2개가 나오면 1000 + 같은눈x100
+# 모드 다른 눈이 나오면 가장 큰 눈 x100
+
+dice = list(map(int, input().split()))
+
+if dice[0] == dice[1] == dice[2]:
+    print(10000 + dice[0]*1000)
+
+elif dice[0] == dice[1] or dice[0] == dice[2]:
+    print(1000 + dice[0] * 100)
+
+elif dice[1] == dice[2]:
+    print(1000 + dice[1] * 100)
+
+else:
+    print(100 * max(dice))
+
