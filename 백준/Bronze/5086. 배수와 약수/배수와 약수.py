b46219ae0843b7f9898893236637c0095d278b73
Dev-nenda <131946370+Dev-nenda@users.noreply.github.com>
2024-03-08 21:31:19 +0900

    [Bronze III] Title: 배수와 약수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.py
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -1,16 +1,12 @@
-# 두 수가 주어진다.
-# 첫 번재 수가 두번째수의 약수일 경우 'factor'를 출력
-# 배수라면 'multiple'을 출력
-# 둘다 아니라면 'neither'를 출력
-
-while True:
-    n1,n2=map(int,input().split())
-
-    if n1==0 and n2==0:
+while (1):
+    x,y = map(int,input().split())
+    
+    if x==0 and y==0:
         break
-    elif n2%n1==0:#약수인 경우
-        print('factor')
-    elif n1%n2==0: #배수인 경우
-        print('multiple')
-    else:#둘 다 아닌 경우
-        print('neither')
+        
+    if x<y and y%x==0:
+        print("factor")
+    elif x>y and x%y==0:
+        print("multiple")
+    else:
+        print("neither")
