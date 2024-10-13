ff53282af7e4b139c70e41e3069b5e51a6fbcd1a
Jooeon Jang <131946370+Dev-nenda@users.noreply.github.com>
2024-10-13 21:17:15 +0900

    [Gold III] Title: 집으로, Time: 56 ms, Memory: 34148 KB -BaekjoonHub

---
+++ 백준/Gold/1069. 집으로/집으로.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+X, Y, D, T = map(int, input().split())
+dist = (X**2 + Y**2) ** 0.5
+
+if D/T <= 1 :
+  print(dist)
+  exit()
+
+direct = dist // D * T
+answer = direct + min(dist % D, T + (D - dist % D))
+answer = min(answer, max(2*T, direct + T))
+print(answer)
